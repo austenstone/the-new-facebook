b309478187e8920acbc25dd35e6b6f2afa43704c
astone2014 <miltonlaxer@gmail.com>
2016-10-02 02:57:21 -0400

    changes

--- index.php
+++ index.php
@@ -9,6 +9,5 @@ if(!isset($_SESSION['id'])){
 <body>
     <h2> You've been successfully logged in </h2>
 </body>
-<footer>
-<p>copyright - Austen Stone & Max 2016</p>
-</footer>
+
+<?php include 'footer.php' ?>
