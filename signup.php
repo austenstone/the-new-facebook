9216436923ab579add637440f7778959cc3234f9
Maksym Shashkun <Maksym Shashkun>
2016-10-02 00:31:01 -0400

    updated signup page

--- signup.php
+++ signup.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'pdoconnection.php';
+require_once 'header.php';
 
 
 
@@ -8,36 +9,38 @@ if(isset($_POST['signup'])){
     $username = $_POST['uname'];
     $password = $_POST['upassword'];
     $email = $_POST['uemail'];
-    echo $username;
-    echo $password;
-    echo $email;
 
-    $insert = mysqli_query($conn, "INSERT INTO users (username, password, email)
-    VALUES ('$username', '$password', '$email')");
-
-    if ($insert) {
-        echo "<h1><center>Successful Registration</center></h1>";
-    } else {
-        printf("Error: %s\n", mysqli_error($conn));
+    $checkusername = mysqli_query($conn, "SELECT * FROM users WHERE username = '$username' LIMIT 1");
+    if ($checkusername -> num_rows > 0){
+        echo "<h1><center>Username already taken</center></h1>";
+    }else{
+        $password_hash = password_hash($password, PASSWORD_BCRYPT);
+        $insert = mysqli_query($conn, "INSERT INTO users (username, password, email)
+        VALUES ('$username', '$password_hash', '$email')");
+        if ($insert) {
+            echo "<h1><center>Successful Registration</center></h1>";
+            echo "<h1><center>Go to Log In page to sign in</center></h1>";
+        } else {
+            printf("Error: %s\n", mysqli_error($conn));
+        }
     }
 }
 
 
 
  ?>
-<form action="signup.php" method="post">
+<!DOCTYPE html>
+
+<form action="signup.php" method="post" id="loginform">
 	<div class="imgcontainer">
 	<div>
 		<img src="">
 	</div>
 	<div class="logincontainer">
-		<p>User Name</p>
-		<input type="text" placeholder="enter username" name="uname">
-		<p>Password</p>
-		<input type="password" placeholder="enter password" name="upassword">
-		<p>Email Adress</p>
-		<input type="text" placeholder="enter email" name="uemail">
-		<button type="submit" name="signup">Signup</button>
-		<input type="checkbox" checked="checked"> Remember me
-	</div>
-</form>
+		<input type="text" placeholder="Username" name="uname" required><br>
+		<input type="password" placeholder="Password" name="upassword" required><br>
+		<input type="text" placeholder="E-mail" name="uemail" required><br>
+		<button type="submit" class="signin"><span>Register</span></button><br><br><br>
+	</form>
+	<a href="login.php">Already have an account?<br><button type="submit" class="signin"><span>Log In</span></button><br></a>
+</div>
