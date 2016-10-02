4a58febd3ffa6102d673af0be54d70b16e70a26c
astone2014 <miltonlaxer@gmail.com>
2016-10-02 00:24:25 -0400

    12.24

--- login.php
+++ login.php
@@ -8,10 +8,11 @@ require_once 'header.php';
 		<img src="">
 	</div>
 	<div class="logincontainer">
-		<input type="text" placeholder="Username" name="uname"><br>
-		<input type="password" placeholder="Password" name="uname"><br>
-		<input type="text" placeholder="E-mail" name="uname"><br>
-		<button type="submit" class="signin">Sign In</button><br>
-		<input type="checkbox" class="checkbox" checked="checked"> Remember me
-	</div>
-</form>
+		<input type="text" placeholder="Username" name="uname" required><br>
+		<input type="password" placeholder="Password" name="uname" required><br>
+		<input type="text" placeholder="E-mail" name="uname" required><br>
+		<button type="submit" class="signin"><span>Sign In</span></button><br>
+		<input type="checkbox" class="checkbox" checked="checked">Remember me<br><br><br>
+	</form>
+	<a href="login.php">Don't have an account Yet?<br><button type="submit" class="signin"><span>Register</span></button><br></a>
+</div>
