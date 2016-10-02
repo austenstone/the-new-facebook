b46b184a65193e2b05e58472053f455b269dfdcd
astone2014 <miltonlaxer@gmail.com>
2016-10-01 22:55:35 -0400

    login updated

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php include pdoconnection.php ?>
+<?php 
+include 'pdoconnection.php';
+include 'header.php';
+?>
+<body>
+</body>
+<footer>
+<p>copyright - Austen Stone & Max 2016</p>
+</footer>
--- login.php
+++ login.php
@@ -1,3 +1,19 @@
 <?php
 require_once 'pdoconnection.php';
  ?>
+<form action="login.php" method="_POST">
+	<div class="imgcontainer">
+	<div>
+		<img src="">
+	</div>
+	<div class="logincontainer">
+		<p>User Name</p>
+		<input type="text" placeholder="enter username" name="uname">
+		<p>Password</p>
+		<input type="password" placeholder="enter username" name="uname">
+		<p>Email Adress</p>
+		<input type="text" placeholder="enter username" name="uname">
+		<button type="submit">Login</button>
+		<input type="checkbox" checked="checked"> Remember me
+	</div>
+</form>
