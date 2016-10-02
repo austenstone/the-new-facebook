cf2ddcd24b23be36bb168373f42bb2e5ffa7f8e8
astone2014 <miltonlaxer@gmail.com>
2016-10-02 13:46:22 -0400

    aligned index

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ if (isset($_POST['status'])){
     VALUES ('$username', '$status_update', '$time')");
 }
 $getstatusupdate = mysqli_query($conn, "SELECT * FROM status WHERE username = '$username' ORDER BY time_stamp DESC");
+?> <div style="text-align:center"><div class="homewrapper"> <?php
 if ($getstatusupdate -> num_rows > 0){
     $row = mysqli_fetch_array($getstatusupdate);
     $last_status = $row["status_update"];
@@ -26,16 +27,17 @@ if ($getstatusupdate -> num_rows > 0){
 
 ?>
 <body>
-    <h2><?php echo "<p>Signed in as " . $_SESSION['id']  . ".</p>" ?></h2>
     	<div>
     	<form action="index.php" method="post">
     		<h3>Update Your Status</h3>
-            <textarea name="status_update" cols="40" rows="5"></textarea>
+            <textarea name="status_update" placeholder="What's going on?" cols="40" rows="5"></textarea><br><br>
     		<button type="submit" name="status"><span>Update</span></button><br><br><br>
     	</form>
     </div>
     <h3>Your latest status</h3>
     <?php echo getstatusupdate($conn, $username); ?>
+	</div>
+	</div>
 </body>
 
 <?php include 'footer.php' ?>
