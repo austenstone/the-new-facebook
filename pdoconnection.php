95e7c1ffa1558a11b4a68cd20e8565f96da5023b
astone2014 <miltonlaxer@gmail.com>
2016-10-01 21:24:25 -0400

    i changed it to make max gay

--- pdoconnection.php
+++ pdoconnection.php
@@ -1,16 +1,16 @@
 <?php
-$servername = "localhost";
-$username = "Thenewfacebook";
-$password = "1234Thenewfacebook";
+$servername = "107.180.26.155";
+$username = "austen_stone";
+$password = "stone";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
-    }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-    }
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+echo "Max is fucking gay";
+
 ?>
