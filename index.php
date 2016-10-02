7bda1cd9c245a2b7d1c259604c52898976336b94
Maksym Shashkun <Maksym Shashkun>
2016-10-02 05:06:01 -0400

    last

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 include 'pdoconnection.php';
 include 'header.php';
+include 'functions.php';
 session_start();
 if(!isset($_SESSION['id'])){
     header("Location: login.php");
@@ -9,8 +10,18 @@ if(!isset($_SESSION['id'])){
 if (isset($_POST['status'])){
     $username = $_SESSION['id'];
     $status_update = $_POST['status_update'];
-    $insert1 = mysqli_query($conn, "INSERT INTO status (username, status_update)
-    VALUES ('$username', '$status_update')");
+    $time = $_SERVER['REQUEST_TIME'];
+    echo $status_update;
+    $insert1 = mysqli_query($conn, "INSERT INTO status (username, status_update, time_stamp)
+    VALUES ('$username', '$status_update', '$time')");
+}
+$getstatusupdate = mysqli_query($conn, "SELECT * FROM status WHERE username = '$username' ORDER BY time_stamp DESC");
+if ($getstatusupdate -> num_rows > 0){
+    $row = mysqli_fetch_array($getstatusupdate);
+    $last_status = $row["status_update"];
+    echo "<h1>QUERRY HAS RESULTS</h1>";
+}else{
+    echo "<h1>NO UPDATES</h1>";
 }
 
 ?>
@@ -23,7 +34,8 @@ if (isset($_POST['status'])){
     		<button type="submit" name="status"><span>Update</span></button><br><br><br>
     	</form>
     </div>
-
+    <h3>Your latest status</h3>
+    <?php echo getstatusupdate($conn, $username); ?>
 </body>
 
 <?php include 'footer.php' ?>
